6dd07fadaf388cb1e5ca52bce9a514a2f17f215a
Kimminu7 <166406778+Kimminu7@users.noreply.github.com>
2024-08-25 21:19:34 +0900

    [level 0] Title: 피자 나눠 먹기 (3), Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.py
+++ 프로그래머스/0/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.py
@@ -1,10 +1,7 @@
 def solution(slice, n):
-    s1,answer = 0,0
+    s1 = 0
     if n % slice == 0:
         s1 = n // slice
-        answer = s1
-    elif n % slice != 0:
-        s1 = n // slice +1
-        answer = s1
-
-    return answer
+    else:
+        s1 = n // slice + 1
+    return s1
