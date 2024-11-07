83963375ad9767cb430ed27d6be7f1765f1c144d
Kimminu7 <166406778+Kimminu7@users.noreply.github.com>
2024-11-07 12:17:26 +0900

    [level 0] Title: 이어 붙인 수, Time: 0.03 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181928. 이어 붙인 수/이어 붙인 수.py
@@ -0,0 +1,11 @@
+def solution(num_list):
+    even = ""
+    odd = ""
+    for i in range(len(num_list)):
+        if num_list[i] % 2 != 0:
+            even += str(num_list[i])
+            
+        elif num_list[i] % 2 == 0:
+            odd += str(num_list[i])
+            
+    return int(even)+int(odd)
