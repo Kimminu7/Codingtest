335572ab5d1e7e97a00b6e20f75e220b969fc1df
Kimminu7 <166406778+Kimminu7@users.noreply.github.com>
2025-03-03 17:04:34 +0900

    [level 1] Title: 평균 구하기, Time: 0.04 ms, Memory: 86.4 MB -BaekjoonHub

--- 프로그래머스/1/12944. 평균 구하기/평균 구하기.java
+++ 프로그래머스/1/12944. 평균 구하기/평균 구하기.java
@@ -1,11 +1,13 @@
 class Solution {
     public double solution(int[] arr) {
         int sum = 0;
+        
         for(int i=0; i<arr.length; i++) {
             sum += arr[i];
         }
-        double arrLength = arr.length;
-        double avg = sum / arrLength;
+        
+        double avg = sum / (double)arr.length;
         return avg;
+        
     }
 }
