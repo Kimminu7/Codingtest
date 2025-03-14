b719d5926040df39e5006cd452a659b2bc876293
Minu Kim <166406778+Kimminu7@users.noreply.github.com>
2025-03-14 10:44:36 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.02 ms, Memory: 84.6 MB -BaekjoonHub

--- 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.java
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.java
@@ -4,10 +4,8 @@ public class Solution {
     public int solution(int n) {
         int answer = 0;
 
-        
         while (n > 0) {
             answer += n % 10;
-            
             n /= 10;
         }
 
