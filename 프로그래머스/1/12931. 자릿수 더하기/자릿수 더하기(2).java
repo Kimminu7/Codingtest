c4421259c2c6369f6dd02a479d516487a72ab101
Minu Kim <166406778+Kimminu7@users.noreply.github.com>
2025-03-14 11:17:46 +0900

    Update 자릿수 더하기(2).java

--- 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기(2).java
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기(2).java
@@ -1,1 +1,12 @@
+public class Solution {
+    public int solution(int n) {
+        String m = String.valueOf(n); // 문자열화 시킴.
+        int answer = 0;
 
+        for (int i=0; i<m.length(); i++) {
+            answer += m.charAt(i) - '0'; // 아스키 코드 값으로 값을 뺌.
+        }
+
+        return answer;
+    }
+}
