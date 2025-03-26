5654cdec5cac40a4028838e775bb8a121249a295
Minu Kim <166406778+Kimminu7@users.noreply.github.com>
2025-03-26 16:36:05 +0900

    Update 하샤드 수.java

--- 프로그래머스/1/12947. 하샤드 수/하샤드 수.java
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.java
@@ -1,19 +1,17 @@
 class Solution {
     public boolean solution(int x) {
         
-        String n = Integer.toString(x);
-        String[] arr = n.split("");
+        String n = Integer.toString(x); // 정수형 -> 문자열로 변환
+        String[] arr = n.split(""); // split으로 쪼개어 배열로 변환
 
-        
         int sum = 0;
         for(int i=0; i<arr.length; i++) {
-            sum += Integer.parseInt(arr[i]);
-        }
-
+            sum += Integer.parseInt(arr[i]); // 문자열 -> 정수형 변환
+        } 
+        
         if (x % sum == 0) {
             return true;
         }
-        
         return false;
     }
 }
