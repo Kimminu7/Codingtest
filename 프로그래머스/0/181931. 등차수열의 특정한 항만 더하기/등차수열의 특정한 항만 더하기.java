57d4c391dddb0bd97355e992e0c2c0bef51feb4f
Minu Kim <166406778+Kimminu7@users.noreply.github.com>
2025-08-16 01:56:35 +0900

    [level 0] Title: 등차수열의 특정한 항만 더하기, Time: 0.03 ms, Memory: 89.6 MB -BaekjoonHub

--- 프로그래머스/0/181931. 등차수열의 특정한 항만 더하기/등차수열의 특정한 항만 더하기.java
+++ 프로그래머스/0/181931. 등차수열의 특정한 항만 더하기/등차수열의 특정한 항만 더하기.java
@@ -2,18 +2,16 @@ class Solution {
     public int solution(int a, int d, boolean[] included) {
         int answer = 0;
         
-        int leng = included.length;
-        
         int startNum = a;
         
-        int[] arr = new int[leng];
+        int[] arr = new int[included.length];
         
-        for(int i=0; i<leng; i++) {
+        for(int i=0; i<included.length; i++) {
             arr[i] = startNum;
             startNum += d;
         }
         
-        for(int i=0; i<leng; i++) {
+        for(int i=0; i<included.length; i++) {
             if(included[i]) {
                 answer += arr[i];
             }
